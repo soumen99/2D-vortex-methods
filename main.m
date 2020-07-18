8631383d3b6bd14f4407d4e97bbdd47fbae5edff
Soumen Chakravarty <2017meb1242@iitrpr.ac.in>
2020-07-19 02:37:29 +0530

    added iterative soln

--- main.m
+++ main.m
@@ -29,18 +29,22 @@ n=round(N/(f*dt));
 t=0:dt:(n-1)*dt;
 %amplitude growth rate
 m=2;
+%wake array
+vortic=zeros(n,2);
 %import kinematics
 [xmain,zmain,vmain,xcoll,zcoll,vcoll,enx,eny,theta,p] = import_paneldata(l,omega,k,t,m,n,h0);
 %number of collocation pts
-n_coll=size(x_main,1)-1;
+n_coll=size(xcoll,1);
 %vortex location wrt inertial coordinates
 vortic(:,1)=l+sx;
 vortic(:,2)=zmain(:,1);
+tau=zeros(n,1);
 %locomotion in x
 sx=-u_inf*t;
+num_iters=10;
 %start time iteration
 for it=1:n
-    
+  
 %A=zeros(n_coll);
 %B_bound=zeros(n_coll);
 %B_sheet=zeros(n_coll,1);
@@ -56,10 +60,17 @@ theta_trail=atan(tan_trail);
 delta=dt*sqrt(u_wake^2+v_wake^2);
 xpanel1=xmain(1);
 xpanel2=xmain(1)+delta/(1+tan_trail^2);
+xmid=(xpanel(1)+xpanel(2))/2;
 zpanel1=zmain(it,1);
 zpanel2=zmain(it,1)+delta*tan_trail/(1+tan_trail^2);
+zmid=(zpanel(1)+zpanel(2))/2;
+    
+
     
 else
+    
+    for iter=1:num_iters 
+        
     it1=it-1;
                                                      
 %wake influence on collocation pts
@@ -75,7 +86,7 @@ rhs2=-(B3+V_stream_n+C_n);
 end
 
 %bound influence coefficients
-[An,At,Bn,Bt]=Influence_coeff(n_coll,it,xcoll,zcoll,xmain,zmain,theta);
+[An,At,Bn,Bt]=Influence_coeff(n_coll,n_coll,it,xcoll,zcoll,xmain,zmain,theta,'bound');
         B=sum(Bn,2);
         %gather terms to form 1st rhs matrix
         rhs1=(p(it)*B_sheetn/delta)-B;
@@ -84,8 +95,38 @@ end
         %setup unsteady kutta condition
         T11=At(1,:)*sol1+sum(Bt(1,:),2)-p(it)*Bsheett(1)/delta;
         Tn1=At(n_coll,:)*sol1+sum(Bt(n_coll,:),2)-p(it)*Bsheett(n_coll)/delta;
-        T12=V_stream_t(1)+(tau(it-1)*Bsheett(1)/delta)+C_t(1);
-        Tn2=V_stream_t(n_coll)+(tau(it-1)*Bsheett(n_coll)/delta)+C_t(n_coll);
+        T12=V_stream_t(1)+(tau(it-1)*Bsheett(1)/delta)+C_t(1)+At(1,:)*sol2;
+        Tn2=V_stream_t(n_coll)+(tau(it-1)*Bsheett(n_coll)/delta)+C_t(n_coll)+At(n_coll,:)*sol2;
+        %setup quadratic coefficients
+        f1 = T11^2-Tn1^2;
+        f2 = 2*(T11*T12-Tn1*Tn2-2/dt);
+        f3 = T12^2-Tn2^2+2*tau(it-1)/dt;
+        s1 = (-f2-sqrt(f2^2-4*f1*f3))/(2*f1);
+        s2 = (f2-sqrt(f2^2-4*f1*f3))/(2*f1);
+        tau(it) = min(s1,s2);
+        
+        q=(tau(it)*sol1/p(it))+sol2;
+        
+        %calculate sheet velocity for next iteration
+        [C_x,C_y] = C_matrix(1,it1,vortic,xmid,zmid,0,tau,it);
+        [A_x,A_y,B_x,B_y]=Influence_coeff(n_coll,1,it,xmid,zmid,xmain,zmain,theta,'sheet');
+        
+        %update parameters for next iteration
+        u_wake = u_wake+A_x*q+(tau(it)/p(it))*sum(B_x,2)+C_x;
+        v_wake = v_wake+A_y*q+(tau(it)/p(it))*sum(B_y,2)+C_y;
+        tan_trail=u_wake/v_wake;
+        theta_trail=atan(tan_trail);
+        delta=dt*sqrt(u_wake^2+v_wake^2);
+        xpanel1=xmain(1);
+        xpanel2=xmain(1)+delta/(1+tan_trail^2);
+        xmid=(xpanel(1)+xpanel(2))/2;
+        zpanel1=zmain(it,1);
+        zpanel2=zmain(it,1)+delta*tan_trail/(1+tan_trail^2);
+        zmid=(zpanel(1)+zpanel(2))/2;
+        
+end
+        
+       %calculate airfoil influence on wake
         
 end 
 
